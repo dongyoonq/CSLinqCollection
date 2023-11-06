8e06255e2882ad8883c17d84ca5586a0ae110896
동윤 김 <ut_community@naver.com>
2023-11-06 13:01:11 +0900

    feat : Add Update
    Update Search Multy

--- CSLinqCollection/Program.cs
+++ CSLinqCollection/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         SearchMultyFly(3);
+        SearchMulty();
     }
 
     public static void SearchMultyFly(int val)
@@ -17,6 +18,17 @@ internal class Program
         foreach (var item in query)
         {
             Console.WriteLine(item);
+        }
+    }
+
+    public static void SearchMulty()
+    {
+        int[] index = Enumerable.Range(1, 10).Where(x => x % 3 == 0).Select((x, y) => y).ToArray(); // Index
+        int[] value = Enumerable.Range(1, 10).Where(x => x % 3 == 0).Select((x, y) => x).ToArray(); // Value
+
+        for (int i = 0; i < index.Length; i++) 
+        {
+            Console.WriteLine($"{index[i]} = {value[i]}");
         }
     }
 }
