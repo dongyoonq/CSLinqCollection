bee31d2784bdc8133ba9b6f27d1c909fb575dcb3
동윤 김 <ut_community@naver.com>
2023-11-06 12:53:12 +0900

    초기 커밋.

---
+++ CSLinqCollection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SearchMultyFly(3);
+    }
+
+    public static void SearchMultyFly(int val)
+    {
+        int[] nums = Enumerable.Range(1, 10).ToArray();
+        var query = from num in nums
+                    where (num % val) == 0
+                    select num;
+
+        foreach (var item in query)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
